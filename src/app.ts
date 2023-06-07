abd7f8a15cae52a2ed9922c27a43aa3b53903c87
adolecumberri <adolfo.reyes@orbitalads.com>
2023-06-07 12:05:00 +0200

    added fastify main server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("Entro")
+import Fastify from 'fastify';
+
+const server = Fastify();
+
+server.get('/', async () => {
+    return { status: 'OK' };
+});
+
+async function start() {
+    try {
+        //0.0.0.0 seen in a tutorial. for Docker connections.
+        await server.listen({
+            port: 3000,
+            host: '0.0.0.0'
+        }); 
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+}
